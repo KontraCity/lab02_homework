0177edb279592b558a628e8041e07c43953e4e1c
KontraCity <nikita2006pro@gmail.com>
2025-04-18 16:51:38 +0300

    Improve comments

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 #include <string>
 
-// Let's pretend that I didn't fix that in the first part of the homework
-// using namespace std;
+// Let's pretend that I didn't fix that in the first part of the homework!
+// Getting rid of this shit --> using namespace std;
 
 int main() {
-    // Get user name
+    // Get user name here!
     std::string name;
     std::getline(std::cin, name);
 
-    // Greet the user
+    // Greet the user now!
     std::cout << "Hello, World from " << name << "!\n";
 }
