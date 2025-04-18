aa42634bb4f4bcf47770c0df336dfac875871681
KontraCity <nikita2006pro@gmail.com>
2025-04-18 16:25:59 +0300

    Minor fixes

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <string>
 
+// Let's pretend that I didn't fix that in the first part of the homework
+// using namespace std;
+
 int main() {
     std::string name;
     std::getline(std::cin, name);
     std::cout << "Hello, World from " << name << "!\n";
-    return 0;
 }
